2248fe2577ac94537fb9f4e9067b572e90d907e8
aeldably <ahmed.eldably@code.berlin>
2024-01-06 16:12:49 +0100

    Creating an NPZ file reader.

--- src/__helpers__/extractor_loader.py
+++ src/__helpers__/extractor_loader.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.lib.npyio import NpzFile
 
 
 def extract_numpy_data(file_path):
@@ -18,10 +19,9 @@ def extract_numpy_data(file_path):
     elif isinstance(data, np.ndarray):
         extracted_data = np.array(data)
     elif isinstance(data, NpzFile):
-        extracted_data = {item['key']: item['value'] for item in data["arr_0"]}
+        extracted_data = np.array([item for item in data["arr_0"]])
+        print(extracted_data)
     else:
         raise Exception("The transformed data saved has to be in a format of numpy array or dict of arrays")
 
     return extracted_data
-
-song = "src/data/processed/train/AClassicEducation_NightOwl/AClassicEducation_NightOwl_MIX.wav.npz"
