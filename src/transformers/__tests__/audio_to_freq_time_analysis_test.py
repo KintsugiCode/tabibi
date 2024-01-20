77b9361e5a4cea193b68d416272dca35e2cc6283
Liam <liam.going@code.berlin>
2024-01-20 16:15:24 +0100

    [tests] test added for audio-to-time-frequency-analysis transformer

---
+++ src/transformers/__tests__/audio_to_freq_time_analysis_test.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pytest
+
+from src.transformers.audio_to_freq_time_analysis import audio_to_freq_time_analysis
+
+
+def test_audio_to_freq_time_analysis_valid_file():
+    # Update the below path to point to an audio .wav file of your choosing.
+    # Expected result is based on real spectrogram data
+    audio_file_path = ("./transformers/__tests__/test_data/AClassicEducation_NightOwl/AClassicEducation_NightOwl_MIX"
+                       ".wav"
+                       )
+    result = audio_to_freq_time_analysis(audio_file_path)
+    assert isinstance(result, np.ndarray)
+
+
+def test_audio_to_freq_time_analysis_invalid_file():
+    # Expect an error when attempting to load a non-audio or non-existent file
+    with pytest.raises(Exception):
+        audio_to_freq_time_analysis('invalid_file_path')
+
+
+def test_audio_to_freq_time_analysis_empty_path():
+    # Expect an exception when attempting to load an empty path
+    with pytest.raises(Exception):
+        audio_to_freq_time_analysis('')
---
+++ src/transformers/audio_to_freq_time_analysis.py
@@ -0,0 +1,22 @@
+import librosa
+import numpy as np
+
+
+def audio_to_freq_time_analysis(file_path):
+    try:
+        # use librosa to load audio file
+        signal, sample_rate = librosa.load(
+            file_path, sr=22050, mono=True
+        )
+
+        # STFT -> spectrogram
+        hop_length = 512  # in num. of samples
+        n_fft = 2048  # window in num. of samples
+        # perform stft
+        stft = librosa.stft(signal, n_fft=n_fft, hop_length=hop_length)
+        # calculate abs values on complex numbers to get magnitude
+        spectrogram = np.abs(stft)
+
+        return spectrogram
+    except Exception as e:
+        raise Exception("Exception occurred: {}".format(e))
