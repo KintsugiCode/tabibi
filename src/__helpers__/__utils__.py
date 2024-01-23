ace38f47ecc7cb961818642f57f69f69094836d5
Liam <liam.going@code.berlin>
2024-01-23 12:06:20 +0100

    [chores] folders cleaned up and duplicate outdated code removed

--- src/__helpers__/__utils__.py
+++ src/__helpers__/__utils__.py
@@ -18,7 +18,7 @@ def get_one_file_with_extension(directory_path, extension):
 
 
 
-def save_numpy_data(file_path, data):
+def savez_numpy_data(file_path, data):
     savez_compressed(file_path, data)
 
 
