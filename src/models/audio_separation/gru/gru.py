6f9e1c10fcc570e07928f34ef57a104b080cd146
Liam <liam.going@code.berlin>
2024-02-01 14:33:54 +0100

    [chores] revert mel-spectogram back to spectogram

---
+++ src/models/audio_separation/gru/gru.py
@@ -0,0 +1,53 @@
+from torch import nn
+import torch
+
+
+class GRU(nn.Module):  # Changed class name to GRU
+    def __init__(self, input_size, hidden_dim, n_layers, output_size, dropout_rate):
+        super(GRU, self).__init__()  # Changed to GRU
+
+        # Number of hidden dimensions
+        self.hidden_dim = hidden_dim
+
+        # Number of hidden layers
+        self.n_layers = n_layers
+
+        # Dropout rate
+        self.dropout_rate = dropout_rate
+
+        # GRU
+        self.gru = nn.GRU(input_size, hidden_dim, n_layers, batch_first=True)
+
+        # Dropout layer
+        self.dropout = nn.Dropout(dropout_rate)
+
+        # Readout layers - Increased capacity with the addition of another fully connected layer
+        self.fc1 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, output_size)
+
+    def forward(self, x):
+        batch_size = x.size(0)
+
+        # Initialize hidden state for first input
+        hidden_i = self.init_hidden(batch_size)
+
+        # Pass in input and hidden state and obtain outputs
+        out, hidden_j = self.gru(x, hidden_i)  # Changed to gru
+
+        # Pass output through dropout layer
+        out = self.dropout(out)
+
+        out = self.fc1(out)
+        out = torch.relu(out)
+        out = self.fc2(out)
+        out = torch.relu(out)
+        out = self.fc3(out)
+
+        return out, hidden_j
+
+    def init_hidden(self, batch_size):
+        # Generates the first hidden state of zeros for the forward pass
+        hidden = torch.zeros(self.n_layers, batch_size, self.hidden_dim)
+
+        return hidden
--- src/visualization/fourier_and_back_trial/fourier_audio_loss_trials.py
+++ src/visualization/fourier_and_back_trial/fourier_audio_loss_trials.py
@@ -18,22 +18,15 @@ def fourier_audio_loss(file_path):
         # STFT -> spectrogram
         hop_length = 512  # in num. of samples
         n_fft = 2048  # window in num. of samples
-        n_mels = 128  # number of mel bands to generate
 
         # perform stft
         stft = librosa.stft(signal, n_fft=n_fft, hop_length=hop_length)
         # calculate abs values on complex numbers to get magnitude
         spectrogram = np.abs(stft)
 
-        # Creating filters
-        mel_scale = librosa.filters.mel(sr=sample_rate, n_fft=n_fft, n_mels=n_mels)
-
-        # Constructing Mel Spectrogram by matrix multiplying STFT with Mel filters
-        mel_spectrogram = np.dot(mel_scale, spectrogram)
-
         t_dict = {"x": list()}
 
-        t_dict["x"].append(mel_spectrogram)
+        t_dict["x"].append(spectrogram)
         norm_x = Normalizer(t_dict["x"])
         t_dict["x"], t_dict["min_max_amplitudes"] = (
             norm_x.normalize(),
