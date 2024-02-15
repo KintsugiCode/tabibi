b1fb80a99c9e880c90c20ae4dd74c9b249757717
Liam <liam.going@code.berlin>
2024-02-15 11:38:17 +0100

    [chores] refactored training/testing code out of main.py

---
+++ src/models/audio_separation/test_separation.py
@@ -0,0 +1,15 @@
+def test_separation(x_test, y_test, model, criterion):
+    print("@@@@@@ Starting model testing @@@@@@")
+    # Switch the model to evaluation mode to turn off features like dropout
+    model.eval()
+
+    # Pass x_test through the model to get y_pred
+    y_pred, _ = model(x_test)
+
+    test_loss = criterion(y_pred, y_test)
+    print(f"Test loss: {test_loss.item():.7f}")
+
+    # Convert tensor back into numpy array and then back to audio
+    y_pred = y_pred.detach().cpu().numpy()
+
+    return y_pred
