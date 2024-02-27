0c3a5b348d1116a09aa8c37c05788ef3a7035be4
Liam <liam.going@code.berlin>
2024-02-27 13:47:23 +0100

    [feature] user input determines whether to train/test new models or just process audio with existing models

--- src/data_manipulation/__helpers__/data_splitter/train_test_split.py
+++ src/data_manipulation/__helpers__/data_splitter/train_test_split.py
@@ -9,6 +9,10 @@ def train_test_splitter(base_path):
             if mix_file_name.endswith(".wav") or mix_file_name.endswith(".flac"):
                 all_file_names.append(foldername)
 
+    # Raise exception if no files found
+    if not all_file_names:
+        raise FileNotFoundError("No files found in the directory.")
+
     # Split into train and test
     train_files, test_files = train_test_split(all_file_names, test_size=0.30)
 
